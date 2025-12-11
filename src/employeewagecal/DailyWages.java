938146eb3b956d0bbb4c8a5a03bae3c124b809e2
abinkgeo <abinkgeo@gmail.com>
2025-12-11 13:03:41 +0530

    added: dailywage

---
+++ src/employeewagecal/DailyWages.java
@@ -0,0 +1,14 @@
+package employeewagecal;
+
+import java.util.Stack;
+
+public class DailyWages {
+
+    static int WAGE_PER_HOUR=20;
+    static int FULL_DAY_HOUR=8;
+    public static void main(String[] args) {
+        int wage=WAGE_PER_HOUR*FULL_DAY_HOUR;
+
+        System.out.println(wage);
+    }
+}
